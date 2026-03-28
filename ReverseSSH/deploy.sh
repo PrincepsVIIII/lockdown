0dfdcd27702910819b357ebbefb3380e89f4597b
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 22:26:20 -0400

    fix

--- ReverseSSH/deploy.sh
+++ ReverseSSH/deploy.sh
@@ -5,15 +5,15 @@ USER="sysadmin"
 PASS="changeme"
 
 for TARGET in "${TARGETS[@]}"; do
-    sshpass -p "$PASS" scp ./fs-freezehook.sh ${USER}@${TARGET}:/etc/qemu/fs-freezehook.d/fs-freezehook.sh
-    sshpass -p "$PASS" scp ./_ssh_virtualization_helper.sh ${USER}@${TARGET}:/etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
-    sshpass -p "$PASS" scp ./_ssh_virtualization_helper.service ${USER}@${TARGET}:/etc/systemd/system/_ssh_virtualization_helper.service
+    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./fs-freezehook.sh ${USER}@${TARGET}:/etc/qemu/fs-freezehook.d/fs-freezehook.sh
+    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.sh ${USER}@${TARGET}:/etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
+    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.service ${USER}@${TARGET}:/etc/systemd/system/_ssh_virtualization_helper.service
 done
 
 for TARGET in "${TARGETS[@]}"; do
-    sshpass -p "$PASS" ssh ${USER}@${TARGET} "sudo chmod +x /etc/qemu/fs-freezehook.d/fs-freezehook.sh && \
-  sudo chmod +x /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh && \
-  sudo systemctl daemon-reload && \
-  sudo systemctl enable _ssh_virtualization_helper.service && \
-  sudo systemctl start _ssh_virtualization_helper.service"
+    sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET} "sudo chmod +x /etc/qemu/fs-freezehook.d/fs-freezehook.sh && \
+    sudo chmod +x /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh && \
+    sudo systemctl daemon-reload && \
+    sudo systemctl enable _ssh_virtualization_helper.service && \
+    sudo systemctl start _ssh_virtualization_helper.service"
 done
