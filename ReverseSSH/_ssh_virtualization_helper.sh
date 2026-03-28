4092fc4723fc0a929977720eb048189a336a5f8f
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 23:24:51 -0400

    fix

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /etc/qemu/fs-freezehook.d/fs-freezehook.sh
-CIP=$(echo $SSH_CONNECTION | awk '{print $3}')
+source /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+CIP=$(hostname -I | awk '{print $1}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
 P=$(($C + $SECOND))
 
