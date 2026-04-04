1c7e99896884972e068b462578a0b01afc8bc675
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-04 18:13:38 -0400

    fix

--- ReverseSSH/send_message.sh
+++ ReverseSSH/send_message.sh
@@ -2,8 +2,7 @@
 
 USER="$1"
 MSG="$2"
-UID=$(id -u "$USER")
 
-CMD="sudo -u $USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$UID/bus /usr/bin/python3 /etc/ufw/applications.d/alerting.py \"$MSG\""
+CMD="sudo -u $USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u)/bus /usr/bin/python3 /etc/ufw/applications.d/alerting.py \"$MSG\""
 
 sudo ./run_cmd.sh "$CMD"
