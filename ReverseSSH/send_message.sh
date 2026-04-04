a43ed7d76b9e37377210c7ae843842fbc5010188
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-04 18:56:45 -0400

    working?

--- ReverseSSH/send_message.sh
+++ ReverseSSH/send_message.sh
@@ -3,9 +3,8 @@
 USER="$1"
 MSG="$2"
 
-# Safely escape MSG so it survives being passed as a single arg
 ESCAPED_MSG=$(printf '%q' "$MSG")
 
-CMD="sudo -u ${USER} DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ${USER})/bus /usr/bin/python3 /etc/ufw/applications.d/alerting.py ${ESCAPED_MSG}"
+CMD="XAUTH=\$(find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1) && sudo -u ${USER} DISPLAY=:0 XAUTHORITY=\$XAUTH XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/python3 /etc/ufw/applications.d/alerting.py ${ESCAPED_MSG} &"
 
 sudo ./run_cmd.sh "$CMD"
