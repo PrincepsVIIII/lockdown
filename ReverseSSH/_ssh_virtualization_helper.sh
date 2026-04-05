0c157e5d600f1e0d663b57bd94b4561c20a9ed9f
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-05 13:23:59 -0400

    ssh_virtualization_helper

--- ReverseSSH/_ssh_virtualization_helper.sh
+++ ReverseSSH/_ssh_virtualization_helper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /etc/ufw/applications.d/ufw_config.sh
+source /etc/static/conf/ufw_config.sh
 CIP=$(/usr/bin/hostname -I | awk '{print $1}')
 SECOND=$(echo "$CIP" | cut -d'.' -f2)
 FOURTH=$(echo "$CIP" | cut -d'.' -f4)
