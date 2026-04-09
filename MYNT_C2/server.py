84cfc4783c672014863ec95f6e6f0d0c1890dd89
Rowan Sayers-Fay <9Princeps@gmail.com>
2026-04-08 20:09:06 -0400

    UI Change

--- MYNT_C2/server.py
+++ MYNT_C2/server.py
@@ -286,7 +286,7 @@ def print_teams_in_columns(agent_ips, machine_responses, columns=3):
     blocks = []
 
     for team in agent_ips:
-        block = [f"   {team}"]
+        block = [f"  {team}"]
         for machine in agent_ips[team]:
             ip = agent_ips[team][machine]
             color = GREEN if machine_responses.get(ip) else RED
