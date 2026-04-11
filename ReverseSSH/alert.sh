3fafa346168385b258269238c18321b72a80edf5
Princep <105245221+Raid-Princep@users.noreply.github.com>
2026-04-11 09:45:33 -0400

    a;ert

---
+++ ReverseSSH/alert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+MSG="$*"
+XAUTH=$(sudo find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1)
+sudo -u sysadmin DISPLAY=:0 XAUTHORITY=$XAUTH XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/python3 /etc/static/conf/alerting.py "$MSG"
--- ReverseSSH/send_message.sh
+++ ReverseSSH/send_message.sh
@@ -5,6 +5,6 @@ MSG="$2"
 
 ESCAPED_MSG=$(printf '%q' "$MSG")
 
-CMD="XAUTH=\$(find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1) && sudo -u ${USER} DISPLAY=:0 XAUTHORITY=\$XAUTH XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/python3 /etc/static/conf/alerting.py ${ESCAPED_MSG} &"
+CMD="XAUTH=\$(find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1) && sudo -u sysadmin DISPLAY=:0 XAUTHORITY=\$XAUTH XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/python3 /etc/static/conf/alerting.py red &"
 
 sudo ./run_cmd.sh "$CMD"
