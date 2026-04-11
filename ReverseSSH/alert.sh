96f90b5739f9edfad5daa80a0ff8542ad007724b
Princep <105245221+Raid-Princep@users.noreply.github.com>
2026-04-11 09:48:08 -0400

    alert

--- ReverseSSH/alert.sh
+++ ReverseSSH/alert.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 MSG="$*"
-XAUTH=$(sudo find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1)
-sudo -u sysadmin DISPLAY=:0 XAUTHORITY=$XAUTH XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/python3 /etc/static/conf/alerting.py "$MSG"
+XAUTH_FILE=$(find /run/user/1000 -name '.mutter-Xwaylandauth.*' 2>/dev/null | head -1)
+export XAUTHORITY=$XAUTH_FILE
+export DISPLAY=:0
+export XDG_RUNTIME_DIR=/run/user/1000
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
+sudo -u sysadmin \
+  DISPLAY=:0 \
+  XAUTHORITY=$XAUTH_FILE \
+  XDG_RUNTIME_DIR=/run/user/1000 \
+  DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+  /usr/bin/python3 /etc/static/conf/alerting.py "$MSG"
